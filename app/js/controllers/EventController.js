db2c2dfc543490c40011e14be89698919330d0a8
NHLeo <leosteffens@usa.net>
2016-12-02 17:09:44 -0500

    Using cookies to track session upvotes

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -1,13 +1,15 @@
 'use strict';
 
 eventsApp.controller('EventController',
-    function EventController($scope, eventData, $log, $anchorScroll){
+    function EventController($scope, eventData, $log, $anchorScroll, $cookieStore){
         $scope.snippet='<span style="color:red">hi there</span>';
         $scope.boolValue = true;
         $scope.mystyle={color:'red'};
         $scope.myclass="blue";
         $scope.buttonDisabled=true;
         $scope.sortorder='name';
+
+        $cookieStore.put ('sessionVotes', {sessions: []});
 
         eventData.getEvent()
             .$promise
@@ -23,13 +25,52 @@ eventsApp.controller('EventController',
             });
         */
 
+
+        $scope.userHasVotedOnSession = function (session) {
+            var sessionVotes = $cookieStore.get('sessionVotes');
+            var hasVoted = false;
+
+            var arrayLen = sessionVotes.sessions.length;
+            for (var i = 0; i < arrayLen; i++) {
+                if (sessionVotes.sessions[i] == session.id) {
+                    hasVoted = true;
+                }
+            }
+
+            console.log("votes: " + sessionVotes.sessions);
+            console.log ("Session id: " + session.id  + " has voted = " + hasVoted);
+
+            return hasVoted;
+        }
+
+        $scope.userHasNotVotedOnSession = function (session) {
+            return !($scope.userHasVotedOnSession(session));
+        }
+
+
         $scope.upVoteSession = function(session) {
-            session.upVoteCount++;
+            if ($scope.userHasNotVotedOnSession(session)) {
+                session.upVoteCount++;
+
+                var sessionVotes = $cookieStore.get('sessionVotes');
+                sessionVotes.sessions.push(session.id);
+                $cookieStore.put ('sessionVotes', sessionVotes);
+
+
+            }
         };
 
         $scope.downVoteSession = function(session) {
-            if (session.upVoteCount > 0) {
-                session.upVoteCount--;
+            if ($scope.userHasNotVotedOnSession(session)) {
+                if (session.upVoteCount > 0) {
+                    session.upVoteCount--;
+
+                    var sessionVotes = $cookieStore.get('sessionVotes');
+                    sessionVotes.sessions.push(session.id);
+                    $cookieStore.put ('sessionVotes', sessionVotes);
+
+
+                }
             }
         };
 
